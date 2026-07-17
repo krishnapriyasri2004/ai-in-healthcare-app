964dd160e39e2b1506a28581c1d004eaaddfe0cf
krishnapriyasri2004 <krishnapriyasri2004@users.noreply.github.com>
2026-07-17 13:17:30 +0530

    refactor: promote organized architecture folders directly to root level and clean up all previous loose source files

--- scratch/check_fbx.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// Just a scratch script to check if we can parse or if we have another file doing so
-console.log("Checking FBX loader rotation characteristics...");
